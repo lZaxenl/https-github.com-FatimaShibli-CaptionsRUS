f799b49591146540712f2befbffe303690eed131
lZaxenl <98841019+lZaxenl@users.noreply.github.com>
2022-10-12 15:01:18 -0500

    Add files via upload

---
+++ captions_r_Us/server.js
@@ -0,0 +1,18 @@
+
+
+const express = require("express"); // imports express library
+const app = express();  // used to listen for request
+const mongoose = require("mongoose"); // helps make the connections between our server and mongodb
+
+// look up youtube on how to setup a simple express server
+
+// look up http request ___
+    // post
+    // get
+
+// app.post is me sending stuff to server
+// app.get is me getting stuff from the server
+
+
+
+
